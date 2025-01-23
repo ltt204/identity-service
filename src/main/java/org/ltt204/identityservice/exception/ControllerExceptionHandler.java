73168799f3efc37977c3227d1b79dd8988313f68
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-01-23 22:12:07 +0700

    feat: enhance exception handling and update user repository for pagination support

--- src/main/java/org/ltt204/identityservice/exception/ControllerExceptionHandler.java
+++ src/main/java/org/ltt204/identityservice/exception/ControllerExceptionHandler.java
@@ -1,28 +1,29 @@
 package org.ltt204.identityservice.exception;
 
-import jakarta.annotation.Resource;
 import org.ltt204.identityservice.exception.customexception.ResourceConflictException;
 import org.ltt204.identityservice.exception.customexception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Objects;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler
-    ResponseEntity<String> handlingRuntimeException(RuntimeException runtimeException) {
-        return ResponseEntity.badRequest().body(runtimeException.getMessage());
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(Objects.requireNonNull(exception.getFieldError()).getDefaultMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(value = ResourceNotFoundException.class)
     ResponseEntity<String> handlingResourceNotFoundException(ResourceNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(value = ResourceConflictException.class)
     ResponseEntity<String> handlingResourceConflictException(ResourceConflictException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
--- src/main/java/org/ltt204/identityservice/repository/UserRepository.java
+++ src/main/java/org/ltt204/identityservice/repository/UserRepository.java
@@ -4,14 +4,12 @@ import org.ltt204.identityservice.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
-
-    Page<User> findAll(Pageable pageable);
-
+public interface UserRepository extends JpaRepository<User, String>, PagingAndSortingRepository<User, String> {
     User getUserById(String id);
 
     boolean existsUsersByUserName(String userName);
