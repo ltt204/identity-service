e8d45ef2c56ad30d7c61c13e1fc0a7ba91c0ff26
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-02-10 20:36:45 +0700

    feat: add date of birth validation and update related dtos

--- src/main/java/org/ltt204/identityservice/dto/request/user/UserCreateRequestDto.java
+++ src/main/java/org/ltt204/identityservice/dto/request/user/UserCreateRequestDto.java
@@ -1,14 +1,17 @@
 package org.ltt204.identityservice.dto.request.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.ltt204.identityservice.validator.DobConstraint;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -23,5 +26,7 @@ public class UserCreateRequestDto {
     String firstName;
     @NotBlank(message = "LASTNAME_REQUIRED")
     String lastName;
+    @NotNull
+    @DobConstraint(min = 18, message = "INVALID_DATE_OF_BIRTH")
     LocalDate dateOfBirth;
 }
