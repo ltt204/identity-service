303ef29984868c934de7ab8dc87efc0a59d3aa75
Trong Tin Ly <trongtin.cv@gmail.com>
2025-03-11 20:57:49 +0700

    feat: implement Redis configuration and update InvalidatedToken entity for Redis support

---
+++ src/main/java/org/ltt204/identityservice/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package org.ltt204.identityservice.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+@ComponentScan
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        var config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(
+                mapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        return new Jackson2JsonRedisSerializer<>(mapper, Object.class);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory,
+            Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer) {
+        var redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
--- src/main/java/org/ltt204/identityservice/entity/InvalidatedToken.java
+++ src/main/java/org/ltt204/identityservice/entity/InvalidatedToken.java
@@ -5,19 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.Date;
 
-@Entity(name = "invalidated_token")
 @Getter
 @Setter
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@RedisHash
 public class InvalidatedToken {
-    @Id
     String token;
-    @Column(name = "expiration_time")
     Date expirationTime;
 }
