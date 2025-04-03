9c2cc39ead04c5279c99a47e371428969a3336b7
ltt204 <22120371@student.hcmus.edu.vn>
2025-04-03 16:11:27 +0700

    feat: implement user event handling with UserCreatedEvent, UserUpdatedEvent, and UserDeletedEvent classes

--- src/main/java/org/ltt204/identityservice/infra/config/RabbitMqConfig.java
+++ src/main/java/org/ltt204/identityservice/infra/config/RabbitMqConfig.java
@@ -7,6 +7,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -101,5 +103,10 @@ public class RabbitMqConfig {
                 .bind(userDeletedQueue())
                 .to(directExchange())
                 .with(getRoutingKey(EVENT_DELETED));
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
