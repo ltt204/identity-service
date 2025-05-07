a633d4b3fbf22985150944c61c8441b8c7a302ae
ltt204 <22120371@student.hcmus.edu.vn>
2025-05-07 11:55:13 +0700

    test: update test

--- src/test/java/org/ltt204/identityservice/controller/JpaUserControllerTest.java
+++ src/test/java/org/ltt204/identityservice/controller/JpaUserControllerTest.java
@@ -3,6 +3,8 @@ package org.ltt204.identityservice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.ltt204.identityservice.application.dtos.user.UserDto;
 import org.ltt204.identityservice.application.services.UserService;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -63,87 +66,93 @@ class JpaUserControllerTest {
                 .build();
     }
 
-    @Test
-    public void createUser_validRequest_success() throws Exception {
-        // GIVEN
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+    @Nested
+    @DisplayName("Test Controller with User Role")
+    @WithMockUser(username = "john", authorities = {"UPDATE_DATA"}, password = "123456789")
+    class TestControllerWithUserRole {
+        @Test
+        public void createUser_validRequest_success() throws Exception {
+            // GIVEN
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
 
-        String content = objectMapper.writeValueAsString(userCreateRequestDto);
+            String content = objectMapper.writeValueAsString(userCreateRequestDto);
 
-        when(userService.create(any())).thenReturn(
-                userDto
-        );
+            when(userService.create(any())).thenReturn(
+                    userDto
+            );
 
-        doNothing().when(rabbitMQEventPublisher).publishEvent(UserCreatedEvent.builder().build());
+            doNothing().when(rabbitMQEventPublisher).publishEvent(UserCreatedEvent.builder().build());
 
-        // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(content))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("code").value(1000)
-                );
+            // WHEN, THEN
+            mockMvc.perform(MockMvcRequestBuilders
+                            .post("/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(content))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("statusCode").value(1000)
+                    );
+        }
+
+        @Test
+        public void createUser_invalidUsername_success() throws Exception {
+            // GIVEN
+            userCreateRequestDto.setUsername("jo");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+
+            String content = objectMapper.writeValueAsString(userCreateRequestDto);
+
+            // WHEN, THEN
+            mockMvc.perform(MockMvcRequestBuilders
+                            .post("/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(content))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("statusCode").value(2002)
+                    );
+        }
+
+        @Test
+        public void createUser_invalidPassword_success() throws Exception {
+            // GIVEN
+            userCreateRequestDto.setPassword("123");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+
+            String content = objectMapper.writeValueAsString(userCreateRequestDto);
+
+            // WHEN, THEN
+            mockMvc.perform(MockMvcRequestBuilders
+                            .post("/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(content))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("statusCode").value(2003)
+                    );
+        }
+
+        @Test
+        public void createUser_invalidDateOfBirth_success() throws Exception {
+            // GIVEN
+            userCreateRequestDto.setDateOfBirth(LocalDate.now());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+
+            String content = objectMapper.writeValueAsString(userCreateRequestDto);
+
+            // WHEN, THEN
+            mockMvc.perform(MockMvcRequestBuilders
+                            .post("/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(content))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("statusCode").value(2004)
+                    );
+        }
     }
 
-    @Test
-    public void createUser_invalidUsername_success() throws Exception {
-        // GIVEN
-        userCreateRequestDto.setUsername("jo");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        String content = objectMapper.writeValueAsString(userCreateRequestDto);
-
-        // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(content))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("code").value(2002)
-                );
-    }
-
-    @Test
-    public void createUser_invalidPassword_success() throws Exception {
-        // GIVEN
-        userCreateRequestDto.setPassword("123");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        String content = objectMapper.writeValueAsString(userCreateRequestDto);
-
-        // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(content))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("code").value(2003)
-                );
-    }
-
-    @Test
-    public void createUser_invalidDateOfBirth_success() throws Exception {
-        // GIVEN
-        userCreateRequestDto.setDateOfBirth(LocalDate.now());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        String content = objectMapper.writeValueAsString(userCreateRequestDto);
-
-        // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(content))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("code").value(2004)
-                );
-    }
 }
